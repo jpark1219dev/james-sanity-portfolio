33a58aed3036d9fe8a7e6620a21a5d011d167b68
jpark1219dev <jpark1219dev@gmail.com>
2022-08-13 15:43:56 -0700

    add base framework to portfolio

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import image from "../images/la_skyline.jpg"
+
+function Home() {
+	return (
+		<main>
+			<img src={image} alt="LA Skyline" className="absolute object-cover w-full h-full"/>
+			<section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+				<h1 className="text-6xl text-green-100 font-bold cursive leading-none lg:leading-snug home-name">Aloha. I'm James.</h1>
+			</section>
+		</main>
+	)
+}
+
+export default Home
